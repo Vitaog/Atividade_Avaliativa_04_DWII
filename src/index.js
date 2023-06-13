8140ec2166b3b2bbad38d415bd5f19def3b18ad3
Jackles kerley <jackles@nexusbr.com>
2023-06-12 21:24:25 -0300

    paginas html

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 const express = require("express")
 const app = express()
+const ejs = require('ejs')
 
 app.set('view engine','ejs')
+app.use(express.static('./public'))
 app.use(express.urlencoded({ extended: true }));
 
 (async () => {
     const database = require('./db');
     await database.sync();
     
+})
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+app.get('/cadastro', (req, res) => {
+    res.render("cadastro")
 })
 
 app.listen(3000, function(erro){
